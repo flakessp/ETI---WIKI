159ca043e475ebf33cd0c33548f07d2357711179
Sergei Panfilov <flakessp@gmail.com>
2015-06-26 16:46:24 +0300

    Added list for page headings

--- client/wiki-final.js
+++ client/wiki-final.js
@@ -1,10 +1,11 @@
-
-
 if (Meteor.isClient) {
+  Template.postList.helpers({
+    'post': function(){
+      return Posts.find();
+    }
+  });
 
-  }
-
-
+}
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
