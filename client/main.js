2c29af03974a5f737fb8652bf5927e9298b5ae35
Sergei Panfilov <flakessp@gmail.com>
2015-07-01 13:10:39 +0300

    Removed autopublish and set up basic publication

--- client/main.js
+++ client/main.js
@@ -1,30 +1,23 @@
-if (Meteor.isClient) {
-  Template.postList.helpers({
-    'post': function(){
-      return Posts.find();
-    }
-  });
-  Template.postPage.helpers({
-    'post': function(){
-      return Posts.find();
-    }
-  });
-  Template.postSubmit.events({
-    'submit form': function(e){
-      e.preventDefault();
-      var post = {
-        title: $(e.target).find('[name=title]').val(),
-        text: $(e.target).find('[name=text]').val()
-      };
-      post._id = Posts.insert(post);
-      Router.go('postPage', post);
-    }
+Meteor.subscribe("posts");
 
-  });
-
-}
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Template.postList.helpers({
+  'post': function() {
+    return Posts.find();
+  }
+});
+Template.postPage.helpers({
+  'post': function() {
+    return Posts.find();
+  }
+});
+Template.postSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    var post = {
+      title: $(e.target).find('[name=title]').val(),
+      text: $(e.target).find('[name=text]').val()
+    };
+    post._id = Posts.insert(post);
+    Router.go('postPage', post);
+  }
+});
